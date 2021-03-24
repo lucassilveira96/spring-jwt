0171cff0816e34a1fc858602f93d5dace0ffafdd
lucassilveira96 <lucas.silveira96@ulbra.edu.br>
2021-03-24 02:11:32 -0300

    initial commit

---
+++ src/main/java/com/silveira/developer/springjwt/models/Role.java
@@ -0,0 +1,20 @@
+package com.silveira.developer.springjwt.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+}
---
+++ src/main/java/com/silveira/developer/springjwt/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.silveira.developer.springjwt.repositories;
+
+import com.silveira.developer.springjwt.models.ERole;
+import com.silveira.developer.springjwt.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
